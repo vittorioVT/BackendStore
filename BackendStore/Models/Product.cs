5a8e58360dbb9627155ba1735c929e426cff7872
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-10-30 10:37:43 +0200

    Added Product class of Model

---
+++ BackendStore/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BackendStore.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public bool IsStore { get; set; }
+        public bool IsStock { get; set; }
+        public int Count { get; set; }
+        public int CountStore { get; set; }
+        public int CountStock { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public int Ordered { get; set; }
+        public decimal Price { get; set; }
+        public string Comment { get; set; }
+
+        [NotMapped]
+        public int Quantity { get; set; }
+    }
+}
--- BackendStore/Models/User.cs
+++ BackendStore/Models/User.cs
@@ -13,6 +13,6 @@ namespace BackendStore.Models
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
+        public bool Admin { get; set; }
     }
 }
