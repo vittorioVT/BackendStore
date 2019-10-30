22a504ec03de025baaa1a0f71cfcd1fd63644e30
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-10-30 10:04:07 +0200

    Added Order class of Model

---
+++ BackendStore/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BackendStore.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public decimal TotalPaid { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int Status { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
