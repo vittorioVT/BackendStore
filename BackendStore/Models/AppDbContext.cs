78286cb87f912f7aec507f07e44377511c628b3b
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-10-31 08:56:49 +0200

    Created class AppDbContext and connectionString

---
+++ BackendStore/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BackendStore.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(): base("DefaultConnection") {}
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
