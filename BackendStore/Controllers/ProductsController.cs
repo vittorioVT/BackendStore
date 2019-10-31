3c8c22091fc3f6f2ceded21ad77c15f595bf31f8
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-10-31 10:02:15 +0200

    Added ProductController for visualization products from database

---
+++ BackendStore/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using BackendStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BackendStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public IHttpActionResult GetProducts()
+        {
+            try
+            {
+                using (var context = new AppDbContext())
+                {
+                    var products = context.Products.ToList();
+                    return Ok(products);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
